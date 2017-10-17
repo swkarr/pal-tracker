b23544f09b6995efd2658850c50956b1ff477233
Shawn Karr <swkarr@hotmail.com>
2017-10-17 15:21:18 -0700

    Add TimeEntry and in memory repository for Entries.

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,41 @@
+package io.pivotal.pal.tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    private Map<Long, TimeEntry> db = new HashMap<>();
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        assert (timeEntry.getId() == 0);
+        long id = db.size() + 1;
+        timeEntry.setId(id);
+        db.put(id, timeEntry);
+        return null;
+    }
+
+    @Override
+    public TimeEntry find(Long id) {
+        return db.get(id);
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        return new ArrayList<>(db.values());
+    }
+
+    @Override
+    public TimeEntry update(Long id, TimeEntry timeEntry) {
+        db.replace(id, timeEntry);
+        timeEntry.setId(id);
+        return timeEntry;
+    }
+
+    @Override
+    public void delete(Long id) {
+        db.remove(id);
+    }
+}
